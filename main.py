ef1abfb3bc4b2ab472f4d6cf9fbf374d3dbf2a9b
simpleReno <ult.ofr.tato@gmail.com>
2024-04-26 20:40:03 -0600

    importing modules to start organize some ideas

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+import subprocess
+import beautifulsoup4 as bs4
+import aiohttp
+import asyncio
+import pandas
+
+# Importing the modules
+
 def main():
 
     pass

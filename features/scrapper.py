209b37bb69641af83818dbfb11d70373e97df59c
simpleReno <ult.ofr.tato@gmail.com>
2024-04-27 20:10:20 -0600

    Change README extension to .md

--- features/scrapper.py
+++ features/scrapper.py
@@ -4,7 +4,20 @@ from bs4 import BeautifulSoup
 import aiohttp
 import asyncio
 
-def main():
+async def get_html(url):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            return await response.text()
+
+async def get_links(html):
+    soup = BeautifulSoup(html, 'html.parser')
+    return [a['href'] for a in soup.find_all('a') if a.has_attr('href')]
+
+async def get_all_links(urls):
+    tasks = [get_html(url) for url in urls]
+    return await asyncio.gather(*tasks)
+
+async def main():
 
     pass
 
